e8ce4f9182b4c7abba44aa2f988fd4372168e943
Gabriel Milani <gabrielwmilani@gmail.com>
2019-06-23 20:12:21 -0300

    Persistir relacionamentos da disciplina

--- api/src/com/ejb/session/DisciplinaSessionBean.java
+++ api/src/com/ejb/session/DisciplinaSessionBean.java
@@ -11,6 +11,8 @@ import javax.persistence.Query;
 import org.jboss.ws.api.annotation.WebContext;
 
 import com.ejb.intf.DisciplinasSession;
+import com.model.ejb.entity.Area;
+import com.model.ejb.entity.Curso;
 import com.model.ejb.entity.Disciplina;
 
 @Stateless
@@ -22,22 +24,62 @@ public class DisciplinaSessionBean implements DisciplinasSession {
 	private EntityManager em;
 	
 	@Override
-	public void remove(Disciplina p) {
-		Disciplina c = em.find(Disciplina.class, p.getId());
+	public void remove(Disciplina d) {
+		Disciplina c = em.find(Disciplina.class, d.getId());
 		if(c != null)
 			em.remove(c);
 	}
 
 	@Override
-	public Disciplina insere(Disciplina c) {
-		em.persist(c);
-		return c;
+	public Disciplina insere(Disciplina d) {
+		persistirRelacionamentos(d);
+		em.persist(d);
+		return d;
 	}
 
 	@Override
-	public Disciplina altera(Disciplina c) {
-		em.merge(c);
-		return c;
+	public Disciplina altera(Disciplina d) {
+		persistirRelacionamentos(d);		
+		em.merge(d);
+		return d;
+	}
+	
+	private void persistirRelacionamentos(Disciplina d) {
+		Curso c = d.getCurso();
+		if (c != null) {
+			boolean criar = true;
+			Long id = c.getId();
+			if (id != null) {
+				Curso curso = em.find(Curso.class, id);
+				if (curso != null) {
+					d.setCurso(curso);
+					criar = false;
+				}				
+			} 
+			
+			if (criar) {
+				em.persist(c);
+				d.setCurso(c);
+			}
+		}
+		
+		Area a = d.getArea();
+		if (a != null) {
+			boolean criar = true;
+			Long id = a.getId();
+			if (id != null) {
+				Area area = em.find(Area.class, id);
+				if (area != null) {
+					d.setArea(area);
+					criar = false;
+				}
+			} 
+			
+			if (criar) {
+				em.persist(a);
+				d.setArea(a);
+			}
+		}		
 	}
 
 	@Override
